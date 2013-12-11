7f4e0c558d57f390f6bb469946735563acd1a263
zsigmond-czine-everit <zsigmond.czine@everit.biz>
2013-12-11 11:20:20 +0100

    Package rename. Refactor the group id (org.everit.db). Create the changeLogPathType to create the correct ResourceAccesor. Write JAVA doc. Logging the process.

---
+++ src/main/java/org/everit/db/lqmg/ChangeLogPathType.java
@@ -0,0 +1,38 @@
+package org.everit.db.lqmg;
+
+/*
+ * Copyright (c) 2011, Everit Kft.
+ *
+ * All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
+/**
+ * The changeLog file path type.
+ */
+public enum ChangeLogPathType {
+
+    /**
+     * The path is classpath. Using if call the maven-plugin the generation.
+     */
+    RESOURCE,
+
+    /**
+     * The path is file system path. Using if call the command line the generation.
+     */
+    FILESYSTEM;
+}
