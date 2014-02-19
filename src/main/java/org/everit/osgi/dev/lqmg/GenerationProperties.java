94f6b064bee86d98b1db91c877dc5e4c89b66051
balazs_zsoldos <balazs_zsoldos@balazs-zsoldos-home-pc>
2014-02-19 15:59:13 +0100

    Releasing 1.0.0

--- src/main/java/org/everit/osgi/dev/lqmg/GenerationProperties.java
+++ src/main/java/org/everit/osgi/dev/lqmg/GenerationProperties.java
@@ -1,26 +1,20 @@
-package org.everit.osgi.dev.lqmg;
-
-/*
- * Copyright (c) 2011, Everit Kft.
+/**
+ * This file is part of Everit - Liquibase-QueryDSL Model Generator.
  *
- * All rights reserved.
+ * Everit - Liquibase-QueryDSL Model Generator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
+ * Everit - Liquibase-QueryDSL Model Generator is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Everit - Liquibase-QueryDSL Model Generator.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+package org.everit.osgi.dev.lqmg;
 /**
  * This class store all parameters which is required to the JAVA classes generation.
  */
